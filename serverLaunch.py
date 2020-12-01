6042d3bfdd76a0287019ad1c6b2cb5789fca8c90
TheMrPuffin <garnera99@gmail.com>
2020-12-01 15:16:56 +0000

    Fixed environmental variable name change.

--- serverLaunch.py
+++ serverLaunch.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import re
 
-subprocess.call(["/home/steam/steamcmd/steamcmd.sh", "+login", os.environ["STEAM_USER"], os.environ["PASSWORD"], "+force_install_dir", "/home/steam/arma3-dedicated", "+app_update", "233780", "validate", "+quit"])
+subprocess.call(["/home/steam/steamcmd/steamcmd.sh", "+login", os.environ["STEAM_USER"], os.environ["STEAM_PASSWORD"], "+force_install_dir", "/home/steam/arma3-dedicated", "+app_update", "233780", "validate", "+quit"])
 
 launch = './arma3server_x64 -name="puffnetserver" -config="./configs/server.cfg" -profiles="./configs/profiles/" -port=2302'
 
